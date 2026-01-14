c6f9af8bc694a9783a5e02426f489ccfed1753c6
yashraj-shri17 <yashraj170803@gmail.com>
2026-01-14 16:22:44 +0530

    Fix ModuleNotFoundError: Use langchain-text-splitters explicit import

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 
